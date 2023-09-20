b58b09f71488fa66cf5b4f80b149a639948d9a0d
Marius Christian Aastebøl <mariusca@hiof.no>
2023-09-20 20:27:42 +0200

    Initial commit

---
+++ app/src/main/java/no/hiof/mariusca/stitur/MainActivity.kt
@@ -0,0 +1,38 @@
+package no.hiof.mariusca.stitur
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import no.hiof.mariusca.stitur.ui.theme.StiTurTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StiTurTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StiTurTheme {
+
+    }
+}
