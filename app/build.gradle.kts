8346343810c1d234369efe470da9a529f593def0
Marius A <89580921+Mariuscaa@users.noreply.github.com>
2023-10-05 12:39:20 +0200

    Revert "Google maps setup"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
     namespace = "no.hiof.mariusca.stitur"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "no.hiof.mariusca.stitur"
@@ -22,16 +21,12 @@ android {
     }
 
     buildTypes {
-        debug {
-            resValue("string", "google_maps_api_key", System.getenv("MAPS_API_KEY"))
-        }
         release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            resValue("string", "google_maps_api_key", System.getenv("MAPS_API_KEY"))
         }
     }
     compileOptions {
@@ -43,7 +38,6 @@ android {
     }
     buildFeatures {
         compose = true
-        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -57,7 +51,6 @@ android {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-compose:2.7.2")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -66,10 +59,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
-    implementation("com.google.maps.android:maps-compose:2.14.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
