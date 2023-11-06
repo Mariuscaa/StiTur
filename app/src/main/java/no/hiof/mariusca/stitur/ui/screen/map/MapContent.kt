39d650770cf8666353e87b79693b34d30cc5d875
Marius A <mariusca@hiof.no>
2023-11-06 13:56:04 +0100

    Split into multiple functions for better readability

--- app/src/main/java/no/hiof/mariusca/stitur/ui/screen/map/MapContent.kt
+++ app/src/main/java/no/hiof/mariusca/stitur/ui/screen/map/MapContent.kt
@@ -1,6 +1,7 @@
 package no.hiof.mariusca.stitur.ui.screen.map
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -8,7 +9,11 @@ import androidx.compose.runtime.MutableState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.CameraPositionState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.JointType
 import com.google.android.gms.maps.model.RoundCap
 import com.google.maps.android.compose.GoogleMap
@@ -16,6 +21,7 @@ import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.Polyline
+import no.hiof.mariusca.stitur.R
 import no.hiof.mariusca.stitur.model.Trip
 
 @Composable
@@ -40,7 +46,7 @@ fun MapContent(
             }
         }) {
 
-        CreateAllTrips(trips, ongoingTripState, cameraPosition, selectedTripState)
+        CreateAllTrips(trips, ongoingTripState, cameraPosition, selectedTripState, context)
 
         Marker(
             MarkerState(position = halden), title = "Halden", snippet = "Marker in Halden."
@@ -69,13 +75,43 @@ private fun CreateNewTrip(
     }
 }
 
+// From https://www.boltuix.com/2022/11/add-custom-marker-to-google-maps-in.html
+fun bitmapDescriptorFromVector(
+    context: Context,
+    vectorResId: Int
+): BitmapDescriptor? {
+
+    // retrieve the actual drawable
+    val drawable = ContextCompat.getDrawable(context, vectorResId) ?: return null
+    drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+    val bm = Bitmap.createBitmap(
+        drawable.intrinsicWidth,
+        drawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
+    )
+
+    // draw it onto the bitmap
+    val canvas = android.graphics.Canvas(bm)
+    drawable.draw(canvas)
+    return BitmapDescriptorFactory.fromBitmap(bm)
+}
+
 @Composable
 private fun CreateAllTrips(
     trips: List<Trip>,
     ongoingTripState: MutableState<Trip?>,
     cameraPosition: CameraPositionState,
-    selectedTripState: MutableState<Trip?>
+    selectedTripState: MutableState<Trip?>,
+    context: Context
 ) {
+    val startIcon = bitmapDescriptorFromVector(
+        context, R.drawable.start
+    )
+
+    val endIcon = bitmapDescriptorFromVector(
+        context, R.drawable.finish
+    )
+
     trips.forEach { trip ->
         val polylinePoints = mutableListOf<LatLng>()
         if (trip.coordinates.isNotEmpty()) {
@@ -84,6 +120,16 @@ private fun CreateAllTrips(
                     LatLng(coordinate.lat.toDouble(), coordinate.long.toDouble())
                 polylinePoints.add(markerPosition)
             }
+            Marker(
+                MarkerState(position = polylinePoints.toList()[0]),
+                title = "Start",
+                icon = startIcon,
+                anchor = Offset(0.5f, 0.5f),
+                onClick = {
+                    selectedTripState.value = trip
+                    true
+                }
+            )
             Polyline(points = polylinePoints.toList(),
                 startCap = RoundCap(),
                 endCap = RoundCap(),
@@ -98,6 +144,15 @@ private fun CreateAllTrips(
                 onClick = {
                     selectedTripState.value = trip
                 })
+            Marker(
+                MarkerState(position = polylinePoints.toList().last()),
+                title = "End",
+                icon = endIcon,
+                onClick = {
+                    selectedTripState.value = trip
+                    true
+                }
+            )
         }
     }
 }
